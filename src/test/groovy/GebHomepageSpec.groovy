1787dbd0a732e40888aef9c50d23a62b6db65249
bartek_moroz <moro.drake@gmail.com>
2018-03-15 14:55:39 +0100

    gradle, spock geb example

---
+++ src/test/groovy/GebHomepageSpec.groovy
@@ -0,0 +1,17 @@
+import geb.spock.GebSpec
+
+class GebHomepageSpec extends GebSpec {
+
+    def "can access The Book of Geb via homepage"() {
+        given:
+        to GebHomePage
+
+        when:
+        manualsMenu.open()
+        manualsMenu.links[0].click()
+
+        then:
+        at TheBookOfGebPage
+    }
+
+}
